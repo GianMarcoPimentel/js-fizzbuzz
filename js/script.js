dccd0e08ed05d953432145d7b64feafef76697e5
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-01-31 15:34:57 +0100

    Fine Esercizio

--- js/script.js
+++ js/script.js
@@ -4,20 +4,44 @@ al posto del numero e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
  */
 
-//creo una lista su html e inserisco gli elementi tramite js c on il ciclo for
+//creo una griglia su html e inserisco gli elementi tramite js c on il ciclo for
 
-//memorizzo la lista
+
+//memorizzo il titolo
+const titleElement = document.querySelector("#title");
+titleElement.innerHTML = "FizzBuzzDom"
+titleElement.style.color = "white";
+titleElement.style.fontSize= "140px"
+titleElement.style.fontWeight="bold";
+titleElement.style.textAlign="center";
+
+
+//memorizzo la griglia
 const grigliaElement = document.querySelector("#griglia")
  // sevolessi scriverlo con js userei questa proprità per farmu apparire il valore in pagina
- //listaElement.innerHTML += "<li>Elemento</li>";
+ //grigliaElement.innerHTML += "<li>Elemento</li>";
 
 
  //uso invece un ciclo for per creare 100 numeri 
- for(i=0; i<=10; i++){
-    grigliaElement.innerHTML += `<div class="square">${i}</div>` ;
+ for(i=1; i<=100; i++){
+   
 
-    /* const newElement = document.createElement("div");
-    newElement.className = "square"
-    newElement.style.background = "red"; */
+            if (i % 3 == 0 && i % 5 == 0){
+            
+                grigliaElement.innerHTML += `
+                <div class="square multiple3-5">fizzbuzz</div>
+                `;
+            } else if (i % 5 == 0){
+                grigliaElement.innerHTML += `<div class="square multiple5">buzz</div>
+                ` ;
+            }else if(i % 3 == 0){
+                grigliaElement.innerHTML += `
+                <div class="square multiple3">fizz</div>
+                `;
+            } else{
+                grigliaElement.innerHTML += `
+                <div class="square normal">${i}</div>
+                `;
+            }
     
 } 
