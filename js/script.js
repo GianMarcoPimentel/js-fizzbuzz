7ef8865f9f29a33b2e7951932d518beb5cae3f5e
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-01-31 14:32:39 +0100

    Creo i 100 numeri col ciclo for

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 //creo una lista su html e inserisco gli elementi tramite js c on il ciclo for
 
+//memorizzo la lista
+const listaElement = document.querySelector(".list")
+ // sevolessi scriverlo con js userei questa proprità per farmu apparire il valore in pagina
+ //listaElement.innerHTML += "<li>Elemento</li>";
+
+
+ //uso invece un ciclo for per creare 100 numeri 
+ for(i=0; i<=100; i++){
+    listaElement.innerHTML += `<li>${i}</li>` ;
+} 
