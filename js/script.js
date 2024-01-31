3aa2f622b8d7c1685157605f02853c16b9282cef
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-01-31 14:52:47 +0100

    aggiungo la classe square e iniizo a stilizzare su css

--- js/script.js
+++ js/script.js
@@ -7,12 +7,17 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 //creo una lista su html e inserisco gli elementi tramite js c on il ciclo for
 
 //memorizzo la lista
-const listaElement = document.querySelector(".list")
+const grigliaElement = document.querySelector("#griglia")
  // sevolessi scriverlo con js userei questa proprità per farmu apparire il valore in pagina
  //listaElement.innerHTML += "<li>Elemento</li>";
 
 
  //uso invece un ciclo for per creare 100 numeri 
- for(i=0; i<=100; i++){
-    listaElement.innerHTML += `<li>${i}</li>` ;
+ for(i=0; i<=10; i++){
+    grigliaElement.innerHTML += `<div class="square">${i}</div>` ;
+
+    /* const newElement = document.createElement("div");
+    newElement.className = "square"
+    newElement.style.background = "red"; */
+    
 } 
